5f29d9459425d96a3d3a19e9a065711b8471a7ce
michaelgsd <mgduncan@graniteschools.org>
2015-06-17 16:12:10 -0600

    inject html and css

--- gulp.config.js
+++ gulp.config.js
@@ -1,8 +1,10 @@
 module.exports = function() {
 	var client = './src/client/';
+    var clientApp = client + 'app/';
+    var temp = './.tmp/';
 
     var config = {
-    	temp: './.tmp/',
+    	temp: temp,
 
         /**
          * File paths
@@ -11,8 +13,32 @@ module.exports = function() {
             './src/**/*.js',
             './*.js'
         ],
+        client: client,
+        css: temp + 'styles.css',
+        index: client + 'index.html',
+        js: [
+            clientApp + '**/*.module.js',
+            clientApp + "**/*.js",
+            '!' + clientApp + '**/*.spec.js'
+        ],
 
-        less: client + 'styles/styles.less'
+        less: client + 'styles/styles.less',
+
+        bower: {
+            json: require('./bower.json'),
+            directory: './bower_components/',
+            ignorePath: '../..'
+        }
+
+    };
+
+    config.getWiredepDefaultOptions = function() {
+        var options = {
+            bowerJson: config.bower.json,
+            directory: config.bower.directory,
+            ignorePath: config.bower.ignorePath
+        };
+        return options;
     };
 
     return config;
